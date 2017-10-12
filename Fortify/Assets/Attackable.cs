9fca64c3ea97e1d5a6b8c8b0981e426a48d56840
Jacob Cohen <JacobCohenIsrael@gmail.com>
2017-10-12 21:28:56 +0300

    First Commit

---
+++ Fortify/Assets/Attackable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attackable : MonoBehaviour {
+
+    public int Hitpoints;
+
+    public void TakeDamage(int damageTaken)
+    {
+        Hitpoints -= damageTaken;
+
+        if (Hitpoints < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Fortify/Assets/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float MoveSpeed;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        transform.position += new Vector3(MoveSpeed * Time.deltaTime, 0, 0);
+    }
+}
