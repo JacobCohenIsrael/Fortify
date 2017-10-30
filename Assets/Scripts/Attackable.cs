e3dddf99236b192dc688109043cc7c89a620dce9
Jacob <JacobCohenIsrael@gmail.com>
2017-10-30 22:28:31 +0200

    Added dying animation

--- Assets/Scripts/Attackable.cs
+++ Assets/Scripts/Attackable.cs
@@ -10,7 +10,14 @@ public class Attackable : MonoBehaviour
 
 		if (HitPoints < 1)
 		{
-			Destroy(gameObject);
+            var animator = GetComponent<Animator>();
+            animator.SetTrigger("isDead");
+            var enemy = GetComponent<Enemy>();
+            if (null != enemy)
+            {
+                enemy.IsMoving = false;
+            }
+			//Destroy(gameObject, 1.5f);
 		}
 	}
 }
